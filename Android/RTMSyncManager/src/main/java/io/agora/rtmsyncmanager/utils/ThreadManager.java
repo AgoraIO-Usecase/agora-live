ce1c5ef00fd62b5fa16b8a136231cd999b29547c
alienzh <zw0872@163.com>
2024-08-05 10:41:53 +0800

    FEAT: update rtm create scene (#82)

--- Android/RTMSyncManager/src/main/java/io/agora/rtmsyncmanager/utils/ThreadManager.java
+++ Android/RTMSyncManager/src/main/java/io/agora/rtmsyncmanager/utils/ThreadManager.java
@@ -2,6 +2,13 @@ package io.agora.rtmsyncmanager.utils;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Process;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Singleton class for managing threads in the Agora RTM Sync Manager.
@@ -12,6 +19,8 @@ public final class ThreadManager {
     private static volatile ThreadManager instance;
 
     private Handler mMainThreadHandler;
+
+    private Executor mIOThreadExecutor;
 
     /**
      * Private constructor for ThreadManager.
@@ -44,6 +53,17 @@ public final class ThreadManager {
      * Initializes the main thread handler.
      */
     private void init() {
+        int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
+        int KEEP_ALIVE_TIME = 1;
+        TimeUnit KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS;
+        BlockingQueue<Runnable> taskQueue = new LinkedBlockingDeque<>();
+        mIOThreadExecutor = new ThreadPoolExecutor(NUMBER_OF_CORES,
+                NUMBER_OF_CORES * 2,
+                KEEP_ALIVE_TIME,
+                KEEP_ALIVE_TIME_UNIT,
+                taskQueue,
+                new InternalThreadFactory(Process.THREAD_PRIORITY_BACKGROUND));
+
         mMainThreadHandler = new Handler(Looper.getMainLooper());
     }
 
@@ -73,5 +93,15 @@ public final class ThreadManager {
      */
     public boolean isMainThread() {
         return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
+
+
+    /**
+     * Switch to an asynchronous thread
+     *
+     * @param runnable
+     */
+    public void runOnIOThread(Runnable runnable) {
+        mIOThreadExecutor.execute(runnable);
     }
 }
