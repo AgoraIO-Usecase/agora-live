4b266a980c42e8aa1421adf38b0f7d2dfbee4ef8
qinhui <>
2024-08-15 18:02:14 +0800

    fix：fix 1.3.0 bugs

--- iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Views/RoomManager/VRRoomCreateView.swift
+++ iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Views/RoomManager/VRRoomCreateView.swift
@@ -25,16 +25,33 @@ public class VRRoomCreateView: UIImageView {
 //        createContainer.layer.shadowColor = UIColor(red: 0, green: 0.55, blue: 0.98, alpha: 0.2).cgColor
 //        createContainer.layer.shadowOpacity = 1
         isUserInteractionEnabled = true
+//        createRoom.setTitleColor(.white, for: .normal)
+//        createRoom.setTitle("voice_create_room".show_localized, for: .normal)
+//        createRoom.setImage(UIImage.sceneImage(name: "show_create_add", bundleName: "VoiceChatRoomResource"), for: .normal)
+//        createRoom.imageEdgeInsets(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5))
+//        createRoom.titleEdgeInsets(UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 0))
+//        createRoom.backgroundColor = .voice_btn_bg
+//        createRoom.titleLabel?.font = .voice_btn_title
+//        createRoom.layer.cornerRadius = 48 * 0.5
+//        createRoom.layer.masksToBounds = true
+//        createRoom.backgroundColor(.red)
+        
         createRoom.imageView?.contentMode = .scaleAspectFit
-        createRoom.setBackgroundImage(UIImage.voice_image("create_room"), for: .normal)
-        createRoom.setImage(UIImage.voice_image("create_room_add"), for: .normal)
+        createRoom.backgroundColor = .voice_btn_bg
+        createRoom.setImage(UIImage.voice_image("show_create_add"), for: .normal)
         createRoom.setTitle("voice_create_room".voice_localized, for: .normal)
         createRoom.titleLabel?.font = UIFont.systemFont(ofSize: 14)
         createRoom.spacingBetweenImageAndTitle = 7
+        createRoom.imageEdgeInsets(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5))
+        createRoom.titleEdgeInsets(UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 0))
+        createRoom.layer.cornerRadius = 48 * 0.5
+        createRoom.layer.masksToBounds = true
         createRoom.accessibilityIdentifier = "voice_chat_create_room_button_title"
         createRoom.translatesAutoresizingMaskIntoConstraints = false
         createRoom.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         createRoom.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        createRoom.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        createRoom.widthAnchor.constraint(equalToConstant: 195).isActive = true
     }
 
     @available(*, unavailable)
