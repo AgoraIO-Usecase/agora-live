f2741280be5c8dd0bc01142638232af719bdc4af
qinhui <>
2024-08-15 18:02:14 +0800

    fix：fix 1.3.0 bugs

---
+++ iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Utils/UIColor+Voice.swift
@@ -0,0 +1,125 @@
+//
+//  Color+Voice.swift
+//  AgoraEntScenarios
+//
+//  Created by qinhui on 2024/8/15.
+//
+
+import Foundation
+extension UIColor {
+    
+    @objc static var voice_main_text: UIColor {
+        return UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    @objc static var voice_beauty_deselect: UIColor {
+        return UIColor(hex: "989DBA", alpha: 1)
+    }
+    
+    @objc static var voice_empty_desc: UIColor {
+        return UIColor(hex: "989DBA", alpha: 1)
+    }
+    
+    // slider
+    @objc static var voice_slider_tint: UIColor {
+        return UIColor(hex: "FFFFFF", alpha: 0.7)
+    }
+    
+    @objc static var voice_zi01: UIColor {
+        return UIColor(hex: "CE27FB", alpha: 1)
+    }
+    
+    @objc static var voice_zi02: UIColor {
+        return UIColor(hex: "684BF2", alpha: 1)
+    }
+    
+    // slider
+    @objc static var voice_zi03: UIColor {
+        return UIColor(hex: "7A59FB", alpha: 1)
+    }
+    
+    @objc static var voice_cover: UIColor {
+        return UIColor(hex: "0C0923", alpha: 0.3)
+    }
+    
+    @objc static var voice_room_info_cover: UIColor {
+        return UIColor(hex: "000000", alpha: 0.25)
+    }
+    
+    @objc static var voice_music_item_bg: UIColor {
+        return UIColor(hex: "000000", alpha: 0.35)
+    }
+    
+    @objc static var voice_dark_cover_bg: UIColor {
+        return UIColor(hex: "151325", alpha: 0.85)
+    }
+    
+    @objc static var voice_btn_bg: UIColor {
+        return UIColor(hex: "345DFF", alpha: 1)
+    }
+
+    @objc static var voice_btn_bg_not_recommended: UIColor {
+        return UIColor(hex: "EFF4FF", alpha: 1)
+    }
+    
+    @objc static var voice_red_dot: UIColor {
+        return UIColor(hex: "FF317B", alpha: 1)
+    }
+    
+    @objc static var voice_chat_user_name: UIColor {
+        return UIColor(hex: "A6C4FF", alpha: 1)
+    }
+    
+    @objc static var voice_chat_input_bg: UIColor {
+        return UIColor(hex: "F1F3F8", alpha: 1)
+    }
+    
+    @objc static var voice_chat_input_text: UIColor {
+        return UIColor(hex: "3C4267", alpha: 1)
+    }
+    
+    @objc static var voice_segment_bg: UIColor {
+        return UIColor(hex: "F2F2F5", alpha: 1)
+    }
+    
+    @objc static var voice_segment_border: UIColor {
+        return UIColor(hex: "ECECF0", alpha: 1)
+    }
+    
+    @objc static var voice_segment_title_nor: UIColor {
+        return UIColor(hex: "979CBB", alpha: 1)
+    }
+    
+    @objc static var voice_footer_separator: UIColor {
+        return UIColor(hex: "F5F4F6", alpha: 1)
+    }
+    
+    @objc static var voice_Ellipse2: UIColor {
+        return UIColor(hex: "E6E5F1", alpha: 1)
+    }
+    
+    @objc static var voice_Ellipse5: UIColor {
+        return UIColor(hex: "6D7291", alpha: 1)
+    }
+    
+    @objc static var voice_Ellipse6: UIColor {
+        return UIColor(hex: "303553", alpha: 1)
+    }
+    
+    @objc static var voice_Ellipse7: UIColor {
+        return UIColor(hex: "040A25", alpha: 1)
+    }
+    
+   
+    @objc static var voice_blue03: UIColor {
+        return UIColor(hex: "1BA1FC", alpha: 1)
+    }
+    
+    @objc static var voice_preset_bg: UIColor {
+        return UIColor(hex: "F0F9FF", alpha: 1)
+    }
+    
+    @objc static var voice_end_bg: UIColor {
+        return UIColor(hex: "141650", alpha: 1)
+    }
+}
