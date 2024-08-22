b7aed5d8742df7f89984fb2ccdbf6a38bb6d2cd7
wushengtao <tamworth@163.com>
2024-08-22 18:33:09 +0800

    fix: optimize token generate

--- iOS/AgoraEntScenarios/Scenes/Show/Models/ShowAgoraKitManager.swift
+++ iOS/AgoraEntScenarios/Scenes/Show/Models/ShowAgoraKitManager.swift
@@ -264,7 +264,6 @@ class ShowAgoraKitManager: NSObject {
             }
             let option = AgoraRtcChannelMediaOptions()
             option.token = token
-            AppContext.shared.rtcToken = token
             self?.updateChannelEx(channelId: channelId, options: option)
         }
     }
