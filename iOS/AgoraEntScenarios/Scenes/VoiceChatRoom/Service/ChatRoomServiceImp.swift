f7c47a71fe3bc96c7a488d38aa0bbbaf5fbecced
HeZhengQing <hezhengqing@agora.io>
2024-12-16 15:49:11 +0800

    [FIX] fix leave room after kick

--- iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Service/ChatRoomServiceImp.swift
+++ iOS/AgoraEntScenarios/Scenes/VoiceChatRoom/Service/ChatRoomServiceImp.swift
@@ -248,11 +248,14 @@ extension ChatRoomServiceImp: VoiceRoomIMDelegate {
     
     public func memberLeave(roomId: String, userName: String) {
         if self.roomServiceDelegate != nil,self.roomServiceDelegate!.responds(to: #selector(ChatRoomServiceSubscribeDelegate.onUserLeftRoom(roomId:userName:))) {
+            if let index = ChatRoomServiceImp.getSharedInstance().mics.firstIndex(where: { $0.member?.name ?? "" == userName}) {
+                ChatRoomServiceImp.getSharedInstance().leaveMic(mic_index: index) { error, mic in
+                }
+            }
             self.mics.first { $0.member?.chat_uid ?? "" == userName }?.member = nil
             self.roomServiceDelegate?.onUserLeftRoom(roomId: roomId, userName: userName)
         }
     }
-    
 }
 
 //MARK: ChatRoomServiceProtocol
@@ -513,11 +516,11 @@ extension ChatRoomServiceImp: ChatRoomServiceProtocol {
     }
     
     func kickOff(mic_index: Int, completion: @escaping (Error?, VRRoomMic?) -> Void) {
-        let mic = VRRoomMic()
-        guard let oldMic = self.mics[safe: mic_index] else { return }
+        guard let mic = self.mics[safe: mic_index] else { return }
         mic.mic_index = mic_index
-        mic.status = (oldMic.status == 2 ? 2:-1)
-        self.cleanUserMicIndex(mic: oldMic)
+        mic.status = (mic.status == 2 ? 2:-1)
+        mic.member = nil
+        self.cleanUserMicIndex(mic: mic)
         VoiceRoomIMManager.shared?.setChatroomAttributes( attributes: ["mic_\(mic_index)":mic.kj.JSONString()], completion: { error in
             if error == nil {
                 self.mics[mic_index] = mic
