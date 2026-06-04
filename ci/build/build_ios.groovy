9d7f468b3707548a18ed0b30352b2d3c039049f5
zhangwei <zhangwei03@shengwang.cn>
2026-06-04 16:21:44 +0800

    fix:update ios ci

--- ci/build/build_ios.groovy
+++ ci/build/build_ios.groovy
@@ -57,4 +57,4 @@ def doPublish(buildVariables) {
     sh "rm -rf *.zip || true"
 }
 
-pipelineLoad(this, "AGORA_LIVE", "build", "ios", "apiexample_mac")
+pipelineLoad(this, "AGORA_LIVE", "build", "ios", "Scenarios-Demo")
